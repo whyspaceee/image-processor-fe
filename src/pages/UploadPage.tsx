aaeb987d024c69a9126831cc38808d447d23822a
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2023-06-06 23:55:55 +0700

    added loading for upload

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -4,12 +4,13 @@ import { useMutation } from "react-query";
 import useUpload from "../hooks/useUpload";
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { Slider } from "@radix-ui/react-slider";
+import Spinner from "../components/ui/spinner";
 
 export default function UploadPage() {
   const loader = useLoaderData() as string;
-  const uploadMutation = useUpload();
+  const { mutate, isLoading } = useUpload();
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    uploadMutation.mutate(acceptedFiles[0]);
+    mutate(acceptedFiles[0]);
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
@@ -18,8 +19,9 @@ export default function UploadPage() {
       className=" flex flex-col items-center justify-center min-h-screen "
       {...getRootProps()}
     >
-      <input {...getInputProps()} />
-      {isDragActive ? (
+      {isLoading ? (
+        <input {...getInputProps()} />
+      ) : isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
         <p>Drag 'n' drop some files here, or click to select files</p>
